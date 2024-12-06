0afdbd0ad044e0804a286a1c7b4d2eb2cb8119bd
Md. Mahdiul Hasan <devmahdimunshee@gmail.com>
2024-12-06 21:50:38 +0600

    Shadcn Install

--- src/app/Components/Helper/SectionHeading.tsx
+++ src/app/Components/Helper/SectionHeading.tsx
@@ -7,7 +7,7 @@ type SectionHeadingProps = {
 
 const SectionHeading = ({ children }: SectionHeadingProps) => {
   return (
-    <h1 className="bg-blue-800 w-fit text-center px-4 py-3 mx-auto text-white font-bold text-2xl sm:text-3xl md:text-4xl uppercase -rotate-6">
+    <h1 className="bg-indigo-800 w-fit text-center px-4 py-3 mx-auto text-white font-bold text-2xl sm:text-3xl md:text-4xl uppercase -rotate-6">
       {children}
     </h1>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,10 @@
-import type { Config } from 'tailwindcss'
-
-export default {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        primary: '#FF6B6B',
-        secondary: '#240b69',
         tertiary: '#0f0715',
         quaternary: '#050709',
         quinary: '#82368C',
@@ -19,8 +13,46 @@ export default {
         octonary: '#33D9B2',
         nonary: '#6A3093',
         denary: '#82368C',
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 2px)`,
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
   plugins: [],
-} satisfies Config
+}
